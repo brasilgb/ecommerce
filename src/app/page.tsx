b64daa4fa87eb74cc0647b8456fc6d9243c38f62
Anderson Brasil <andersonbrasil72@gmail.com>
2024-01-08 15:48:18 -0300

    Push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   const router = useRouter();
   useEffect(() => {
-    if(authenticated) {
+    if(!authenticated) {
       return router.push('/');
     }
   },[authenticated, router]);
