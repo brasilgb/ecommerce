b33223a8c70df5f5a40c8b1dabcb8d9bef1eaf35
Anderson Brasil <andersonbrasil72@gmail.com>
2024-01-09 09:58:52 -0300

    push

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,10 +1,15 @@
 'use client'
 import React from 'react'
 import Image from "next/image"
+import Link from "next/link"
 
 const Logo = () => {
   return (
+    <Link
+    href="http://portal.gruposolar.com.br"
+    >
     <Image src={require('@/assets/images/logo_grupo_blue.png')} alt={"Logo"} height={40} />
+    </Link>
   )
 }
 
