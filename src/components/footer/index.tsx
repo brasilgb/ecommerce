aca05b3c0b26a0c48103b522f4acc5999c37c772
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-06-06 14:14:50 -0300

    Push

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center py-0.5 bg-solar-blue-secundary">
+    <div className="flex items-center justify-center py-0.5 bg-solar-blue-primary">
       <p className="md:text-xs text-[8px] text-solar-gray-light">
         &copy; {moment().format('YYYY')} Solar Comércio e Agroindústria Ltda.
       </p>
